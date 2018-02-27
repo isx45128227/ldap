c75a9dcb5ab1c51d4b95a8f501f77cb529c87235
RogerF <roogeer96@gmail.com>
2018-02-27 11:46:35 +0100

    ldapserver

--- ldapserver:dataDB/startup.sh
+++ ldapserver:dataDB/startup.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
 /usr/bin/echo "slapd OK"
-/usr/sbin/slapd -d0 -u ldap -h "ldap:/// ldaps:/// ldapi:///"
+# la opcio -d0 fa que s'executi en primer pla, per tant podrem fer que el docker treballi com un deamon
+/usr/sbin/slapd -d1 -u ldap -h "ldap:/// ldaps:/// ldapi:///"
 
