cb033572b64c8cac26ea2c7ebbcd4abd6db3d020
isx45128227 <35956939+isx45128227@users.noreply.github.com>
2018-03-05 14:37:44 +0100

    delete usuaris-mes

--- ldapserver:dataDB/install.sh
+++ ldapserver:dataDB/install.sh
@@ -5,6 +5,5 @@ rm -rf /var/lib/ldap/*
 cp /usr/share/openldap-servers/DB_CONFIG.example /var/lib/ldap/DB_CONFIG
 slaptest -f /opt/docker/slapd.conf -F /etc/openldap/slapd.d
 slapadd -F /etc/openldap/slapd.d -l /opt/docker/organitzacio-edt.org.ldif
-slapadd -F /etc/openldap/slapd.d -l /opt/docker/usuaris-edt.org.ldif
 chown -R ldap.ldap /etc/openldap/slapd.d/
 chown -R ldap.ldap /var/lib/ldap/
